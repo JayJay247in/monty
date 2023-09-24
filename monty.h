0c8df2d8c724947ef5e3f12db530f2aa1ddcb6ec
JayJay247in <cj193532@gmail.com>
2023-09-23 23:55:16 -0700

    1. pint

--- monty.h
+++ monty.h
@@ -41,5 +41,6 @@ extern unsigned int line_number;
 /* Prototypes */
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
