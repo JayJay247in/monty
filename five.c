370e1528d95a14f0b33592cf01cfd9f575913d3f
JayJay247in <cj193532@gmail.com>
2023-09-24 00:58:02 -0700

    5. nop

---
+++ five.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - Doesn't do anything
+ * @stack: Double pointer to the head node of the stack
+ * @line_number: Line number of the opcode in the file
+ *
+ * Return: void
+*/
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+}
